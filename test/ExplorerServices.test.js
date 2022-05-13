ab48f8d07590e9eb1cbda41928a40e496f288fe3
FernandoGR <gamboa_029@hotmail.com>
2022-05-12 19:17:39 -0500

    W4E1V6

--- test/ExplorerServices.test.js
+++ test/ExplorerServices.test.js
@@ -2,26 +2,23 @@ const Reader = require("./../lib/utils/Reader")
 const ExplorerService = require("./../lib/services/ExplorerServices")
 
 describe("Unit Test for Services", () =>{
-    test('1) filterByMission', () =>{
-        const explorer = [{"name": "Woopa1","githubUsername": "ajolonauta1","score": 1,"mission": "java"},
+    const explorer = [{"name": "Woopa1","githubUsername": "ajolonauta1","score": 1,"mission": "java"},
             {"name": "Woopa2","githubUsername": "ajolonauta2","score": 2,"mission": "node"},
             {"name": "Woopa3","githubUsername": "ajolonauta3","score": 3,"mission": "python",}]
-        
+
+    test('1) filterByMission', () =>{      
         const explorersInNode = ExplorerService.filterByMission(explorer, "node")
         const prueba = [{"name": "Woopa2","githubUsername": "ajolonauta2","score": 2,"mission": "node"}]
         expect(explorersInNode).toStrictEqual(prueba)
     })
     test('2) getAmountOfExplorersByMission', () =>{
-        const explorers = Reader.readJsonFile("explorers.json")
-        const userInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        const userInNode = ExplorerService.getAmountOfExplorersByMission(explorer, "node");
 
-        expect(userInNode).toBe("Cantidad de Explorers en mision Node: 10")
+        expect(userInNode).toBe("Cantidad de Explorers en mision Node: 1")
     })
     test('3) getExplorersUsernamesByMission', () =>{
-        const explorers = Reader.readJsonFile("explorers.json")
-        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-        const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+        const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorer, "node")
 
-        expect(usernamesInNode).toStrictEqual(["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"])
+        expect(usernamesInNode).toStrictEqual("Los Username son: ajolonauta2")
     })
 });
